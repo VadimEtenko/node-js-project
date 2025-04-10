ece55c4a74d66e75c063f5c7818152e18288fb35
Vadim <etenkovadim@gmail.com>
2025-04-10 18:18:02 +0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+'use strict'
+
+import * as http from 'http'
+import * as fs from 'fs';
+import log4js from "log4js";
+import crypto from 'crypto';
+
+log4js.configure({
+    appenders: { text: { type: "file", filename: "text.log" } },
+    categories: { default: { appenders: ["text"], level: "info" } },
+});
+
+const logger: log4js.Logger = log4js.getLogger();
+
+logger.level = "info";
+
+
+const server: http.Server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+        const uuid:string = crypto.randomUUID();
+
+        logger.info(`Get request for ${req.url} with ${req.method} method with id=${uuid}`);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        const html:string = fs.readFileSync('static/html/index.html', 'utf-8').toString();
+
+        res.write(html);
+        res.end();
+        logger.info(`Send 'hello world' for request with id=${uuid}`);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Page Not Found!');
+        res.end();
+    }
+})
+
+server.listen(3000, () => {
+    console.log("Сервер запущен по адресу http://localhost:3000");
+});
